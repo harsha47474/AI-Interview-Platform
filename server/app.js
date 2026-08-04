2e9baf33a4b820635bf3ba3d174ba8282be72b5c
Harsha <harsha1127achu@gmail.com>
2026-08-05 00:59:54 +0530

    Authentication controllers and Routes setup | generating token and cookie

--- server/app.js
+++ server/app.js
@@ -1,15 +1,29 @@
-import express from 'express'
-import 'dotenv'
-import connectDB from './config/connectDB.js';
+import express from "express";
+import "dotenv";
+import connectDB from "./config/connectDB.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import authRouter from "./routes/auth.route.js";
 
 const app = express();
-const PORT = process.env.PORT || 8080
+app.use(
+  cors({
+    origin: "http://localhost:5173/",
+    credentials: true,
+  }),
+);
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRouter);
+
+const PORT = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
-    res.send("This is my new Project. Working on an AI interview platform");
+  res.send("This is my new Project. Working on an AI interview platform");
 });
 
-app.listen(PORT, ()=>{
-    console.log(`Server listening to port ${PORT}`);
-    connectDB();
+app.listen(PORT, () => {
+  console.log(`Server listening to port ${PORT}`);
+  connectDB();
 });
