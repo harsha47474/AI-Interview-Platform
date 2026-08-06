3bdbd4a43495d8ea747e6de4d3781f88e2ec5520
Harsha <harsha1127achu@gmail.com>
2026-08-06 23:35:23 +0530

    Server auth middleware | current user fetch

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ import connectDB from "./config/connectDB.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import authRouter from "./routes/auth.route.js";
+import userRouter from "./routes/user.route.js";
 
 const app = express();
 
@@ -17,6 +18,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
 
 const PORT = process.env.PORT | 3000;
 
---
+++ server/middlewares/isAuth.js
@@ -0,0 +1,24 @@
+import jwt from "jsonwebtoken";
+
+export const isAuth = (req, res, next) => {
+    try {
+        const { token } = req.cookies;
+
+        if (!token) {
+            return res.status(401).json({
+                message: "User not authenticated",
+            });
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        req.userId = decoded.userId;
+
+        next();
+    } catch (error) {
+        console.error(error);
+        return res.status(401).json({
+            message: "Invalid token",
+        });
+    }
+};
