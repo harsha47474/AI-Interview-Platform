d253544b54ffe15c7043a70c9eb184960e68e3d7
Harsha <harsha1127achu@gmail.com>
2026-08-06 23:35:45 +0530

    Get User on website request

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,30 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { redirect, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Auth from "./pages/Auth";
+import axios from "axios";
+import { useEffect } from "react";
 
 const App = () => {
+  useEffect(() => {
+    const getUser = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:3000" + "/api/user/current-user",
+          {
+            withCredentials: true,
+          },
+        );
+
+        console.log(response.data);
+      } catch (error) {
+        console.log(error.response?.status);
+        console.log(error.response?.data);
+      }
+    };
+
+    getUser();
+  }, []);
   return (
     <Routes>
       <Route path="/" element={<Home />} />
